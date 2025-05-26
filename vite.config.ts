ff673c53b40535f91754a1898522f90d5f4adc49
Dingsdah <51045811+janwalenda@users.noreply.github.com>
2025-05-26 17:19:54 +0200

    refactor(config): removed unused configuration an package

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,6 @@
 // https://vite.dev/config/
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifestFilename: 'easycrypt.manifest',
-      manifest: {
-        id: "https://easycrypt.janwalenda.de/",
-        lang: "en-US",
-        description: "EasyCrypt is a simple and secure way to encrypt your text.",
-        name: 'EasyCrypt',
-        short_name: 'EC',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#588157',
-        theme_color: '#588157',
-      }
-    }),
-  ],
 })
