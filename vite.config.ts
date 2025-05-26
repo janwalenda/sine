06fbde6dfe2ae94a3adcebfde271f985459a55ee
Dingsdah <51045811+janwalenda@users.noreply.github.com>
2025-05-26 17:12:32 +0200

    refactor(js): from deno to node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
-
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [deno()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifestFilename: 'easycrypt.manifest',
+      manifest: {
+        id: "https://easycrypt.janwalenda.de/",
+        lang: "en-US",
+        description: "EasyCrypt is a simple and secure way to encrypt your text.",
+        name: 'EasyCrypt',
+        short_name: 'EC',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#588157',
+        theme_color: '#588157',
+      }
+    }),
+  ],
 })
